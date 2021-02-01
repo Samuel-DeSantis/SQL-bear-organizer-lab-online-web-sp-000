4bf497238db4ebcca88a08f5bbe4c1dddda48809
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-01 05:50:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,6 +11,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Grinch", 5, "Male", "Brown", "Cool", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Wendy", 5, "Female", "Brown", "Cool", 1);
+  VALUES ("Wendy", 5, "Female", "Brown", "Goofy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES (NULL, 5, "Male", "Brown", "Cool", 1);
